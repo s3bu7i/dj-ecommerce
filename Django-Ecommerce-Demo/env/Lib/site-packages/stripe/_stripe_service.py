d5d336893515858320a1d1e2a0e6f902a27376e8
Sabuhi Gasimzada <sabuhi.qasimzada2021@khazar.org>
2024-02-17 15:32:57 +0400

    *

--- Django-Ecommerce-Demo/env/Lib/site-packages/stripe/_stripe_service.py
+++
@@ -1,8 +0,0 @@
-from stripe._api_requestor import _APIRequestor
-
-
-class StripeService(object):
-    _requestor: _APIRequestor
-
-    def __init__(self, requestor):
-        self._requestor = requestor

0fd2b14ae06e4674735090f663ceed4aeeb3a9a0
Sabuhi Gasimzada <sabuhi.qasimzada2021@khazar.org>
2024-02-19 16:24:02 +0400

    *

--- demo/settings.py
+++ demo/settings.py
@@ -83,6 +83,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
+
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
